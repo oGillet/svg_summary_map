c451b78323f8c3b74c2fc708fdc0dd8b2691ba6a
Olivier <gillet.olivier@outlook.fr>
2021-05-20 11:43:58 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'hhttps://docs.google.com/spreadsheets/d/1mSVoBU88Uwtkc8wkpf3IaDBwOG9-rGQ8tz688kX9mh4/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyCclDqrRuiqQHpPIgnlj8gq0ZckRkR8tgw';
